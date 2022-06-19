8ae8b5d2f079504d84d3d3f77f0a1a019a33ee7c
teung22 <ala01950195@gmail.com>
2022-06-20 03:20:07 +0900

    test

---
+++ 02test.py
@@ -0,0 +1,9 @@
+title = "여름날"  # '문자열'   "문자열"
+city = "seoul"
+age = 23
+avg = 78.9
+
+#02test.py 문서 
+print(title)
+print(city)
+print('-' * 100) #빈줄 
---
+++ 03testinput.py
@@ -0,0 +1,8 @@
+title = input('계절 >>> ') #입력  input('안내문')
+city = input('도시 >>> ')  #입력  input('안내문')
+
+#03testinput.py 문서
+print() 
+print(title)
+print(city)
+print('-' * 100) #빈줄 
